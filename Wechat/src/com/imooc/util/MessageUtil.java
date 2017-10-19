18132f8bdb0da1c2e228b18aaa2f23d74cf06eac
cjy <392026769@qq.com>
2017-10-19 19:25:05 +0800

    10.19 wechat第一次更新

---
+++ Wechat/src/com/imooc/util/MessageUtil.java
@@ -0,0 +1,103 @@
+package com.imooc.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import com.imooc.po.News;
+import com.imooc.po.NewsMessage;
+import com.imooc.po.TextMessage;
+import com.thoughtworks.xstream.XStream;
+
+
+
+
+
+
+public class MessageUtil {
+	public static final String MESSAGE_TEXT = "text";
+	public static final String MESSAGE_IMAGE = "image";
+	public static final String MESSAGE_VOICE = "voice";
+	public static final String MESSAGE_VIDEO = "video";
+	public static final String MESSAGE_LINK = "link";
+	public static final String MESSAGE_LOCATION = "location";
+	public static final String MESSAGE_EVENT = "event";
+	public static final String MESSAGE_SUBSCRIBE = "subscribe";
+	public static final String MESSAGE_UNSUBSCRIBE = "unsubscribe";
+	public static final String MESSAGE_CLICK = "click";
+	public static final String MESSAGE_VIEW = "view";
+	public static final String MESSAGE_NEWS = "news";
+	
+	
+	public static Map<String, String> xmlToMap(HttpServletRequest request) throws Exception{
+		Map<String, String> map = new HashMap<String,String>();
+		SAXReader reader = new SAXReader();
+		InputStream ins = request.getInputStream();
+		Document doc = reader.read(ins);
+		Element root = doc.getRootElement();
+		List<Element> list = root.elements();
+		for(Element e:list)
+			map.put(e.getName(), e.getText());
+		ins.close();
+		return map;
+	}
+	public static String textMessageToXml(TextMessage textMessage){
+		XStream xstream = new XStream();
+		xstream.alias("xml", textMessage.getClass());
+		return xstream.toXML(textMessage);
+		
+	}
+	/*
+	 * 主菜单
+	  */
+	public static String subScribeMenu(){
+		StringBuffer sb= new StringBuffer();
+		sb.append("欢迎您的关注，回复影视关键词即可在线观看");
+		return sb.toString();
+		
+	}
+	
+	public static String newsMessageToXml(NewsMessage newsMessage){
+		XStream xStream = new XStream();
+		xStream.alias("xml", newsMessage.getClass());
+		xStream.alias("item",new News().getClass());
+		return xStream.toXML(newsMessage);
+		
+	}
+	
+	public static String packNewsMessage(String toUserName,String fromUserName,List<News> newsList){
+		String message = null;
+		NewsMessage newsMessage = new NewsMessage();
+		newsMessage.setToUserName(fromUserName);
+		newsMessage.setFromUserName(toUserName);
+		newsMessage.setCreateTime(new Date().toString());
+		newsMessage.setMsgType(MESSAGE_NEWS);
+		newsMessage.setArticleCount(newsList.size());
+		newsMessage.setArticles(newsList);
+		message = newsMessageToXml(newsMessage);
+		return message;
+	}
+	
+	public static String packText(String toUserName,String fromUserName,String reply){
+		TextMessage text = new TextMessage();
+		text.setFromUserName(toUserName);
+		text.setToUserName(fromUserName);
+		text.setMsgType("text");
+		text.setCreateTime(new Date().toString());
+		text.setContent(reply);
+		return MessageUtil.textMessageToXml(text);
+		
+	}
+}
