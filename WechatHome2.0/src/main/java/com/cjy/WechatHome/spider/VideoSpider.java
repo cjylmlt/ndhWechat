9c7cd10a602af5eaed498ecead9158bae1507d21
cjy <392026769@qq.com>
2017-11-08 22:07:31 +0800

    热搜榜实现异步

--- WechatHome2.0/src/main/java/com/cjy/WechatHome/spider/VideoSpider.java
+++ WechatHome2.0/src/main/java/com/cjy/WechatHome/spider/VideoSpider.java
@@ -44,7 +44,6 @@ public class VideoSpider {
 	
 		//get.addRequestHeader("User-Agent","Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36");
 		try {
-			
 			int status = client.executeMethod(get);
 			get.getParams().setContentCharset("UTF8");
 			InputStream inputStream = get.getResponseBodyAsStream();  
